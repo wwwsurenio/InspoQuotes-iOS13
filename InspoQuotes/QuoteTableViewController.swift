62f2b3819e80dbe491ffdb26e2ccf18e5ffc93ce
Suren <105368039+wwwsurenio@users.noreply.github.com>
2023-02-26 13:02:53 +0100

    removed the extra cell when the premium is already purchased

--- InspoQuotes/QuoteTableViewController.swift
+++ InspoQuotes/QuoteTableViewController.swift
@@ -45,8 +45,11 @@ class QuoteTableViewController: UITableViewController, SKPaymentTransactionObser
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return quotesToShow.count + 1
+        if isPurchased(){
+            return quotesToShow.count
+        }else{
+            return quotesToShow.count + 1
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
